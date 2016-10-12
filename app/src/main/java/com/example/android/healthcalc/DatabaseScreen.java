a4c623e81bec749315f7c66a7f483363d00d70ed
StanislavF <stanislavfotev95@gmail.com>
2016-10-12 22:21:56 +0300

    adding elements to database_screen layout

--- app/src/main/java/com/example/android/healthcalc/DatabaseScreen.java
+++ app/src/main/java/com/example/android/healthcalc/DatabaseScreen.java
@@ -1,19 +1,49 @@
 package com.example.android.healthcalc;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class DatabaseScreen extends AppCompatActivity {
 
+    private EditText mEditText;
     DatabaseHelper databaseHelper;
+    Context ctx=this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_database_screen);
+
+
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+
+
+        mEditText=(EditText)findViewById(R.id.edit_text);
+        mEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                Toast.makeText(ctx,"OnTextChanged",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                Toast.makeText(ctx,"afterTectChanged",Toast.LENGTH_SHORT).show();
+            }
+        });
 
         databaseHelper = new DatabaseHelper(this);
         int testInt = 10;
